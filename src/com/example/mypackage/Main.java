7d3861181b4aba4568293b80d5c0723263dedb9f
homide <55741867+homide@users.noreply.github.com>
2020-05-03 22:09:05 +0530

    dice made

--- src/com/example/mypackage/Main.java
+++ src/com/example/mypackage/Main.java
@@ -3,32 +3,37 @@ package com.example.mypackage;
 import java.util.Random;
 import java.util.Scanner;
 
-interface goti{
-
-}
+//class goti{
+//    public void
+//}
 
 public class Main {
 
+
     //This will give us a random number
-    public int dice(String go){
-        int[] dice_numbers = {1,2,3,4,5,6};
-        if(go.equals("go")){
+    public int dice(String cmd) {
+        int[] dice_numbers = {1, 2, 3, 4, 5, 6};
+        if(cmd.equals("go")){
             int num_returned = new Random().nextInt(dice_numbers.length);
             return dice_numbers[num_returned];
         }
         else{
             return 0;
         }
+
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        boolean name = true;
         Main main = new Main();
-        String cmd = sc.nextLine();
-        if(cmd.equals("start")) {
-            for (int i = 0; i <10 ; i++){
-                String cmd2 = sc.nextLine();
-                System.out.println(main.dice(cmd2));
+        while(name){
+            Scanner sc = new Scanner(System.in);
+            String cmd = sc.nextLine();
+            if(cmd.equals("go")){
+                System.out.println(main.dice(cmd));
+            }
+            else{
+                break;
             }
         }
     }
